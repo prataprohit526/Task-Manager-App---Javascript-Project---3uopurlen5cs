ddd3f89a27d5ca534bb01adbe0213aabd8721858
prataprohit526 <121006619+prataprohit526@users.noreply.github.com>
2023-02-10 18:10:38 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ const addTask = document.querySelectorAll(".add-task");
 const all_status = document.querySelectorAll(".open-box");
 let dragMe = null;
 
+let data = [
+ 
+]
+
 addTask.forEach((ele) => {
   ele.addEventListener("dragstart", dragStart);
   ele.addEventListener("dragend", dragEnd);
@@ -46,49 +50,113 @@ function dragDrop() {
 }
 /* -----------Add new Task--------------------*/
 
+document.querySelector('#add-task').addEventListener("click", createTask);
 function createTask(){
-  let check=document.getElementById("inpuText").value;
-  if(check!=""){
+  let title = document.getElementById("inpuText").value.trim();
+  if(title === "")  return;
   /* -----New Div----- */
   let div=document.createElement("div");
-  let addPreviousInput=document.getElementById("inpuText").value;
-  let text=document.createTextNode(addPreviousInput);
-
+  let uniqueId = randomId()
+  div.dataset.id= uniqueId;
   div.classList.add("add-task");
   div.setAttribute("draggable","true");
   div.setAttribute("id", "drag-item-list");
-  div.setAttribute("onclick","show()");
+  div.addEventListener('click', (e) => {
+    showWithDetails(e.target)
+  });
   /*------New Hr Line------ */
   let hr=document.createElement("hr");
   hr.setAttribute("id","hr-line");
   div.appendChild(hr);
   /*----------New P Tag------------- */
   let p=document.createElement("p");
-    p.setAttribute("id","drag-item");
-   p.appendChild(text);
-   div.appendChild(p);
-   /*----------Add Circle Icon-------------- */
-   let icon=document.getElementById("font-awesome");
-    icon = icon.cloneNode(true);
-   div.appendChild(icon);
-   no_status.appendChild(div);
+  p.setAttribute("id","drag-item");
+  p.textContent = title;
+  div.appendChild(p);
+  /*----------Add Circle Icon-------------- */
+  let icon = document.createElement("i");
+  icon.classList.add('fa-circle');
+  icon.classList.add('fa-regular');
+  div.appendChild(icon);
+  icon.id = `icon-${uniqueId}`
+  let icon1=document.createElement("i");
+  icon1.classList.add('fa-solid');
+  icon1.classList.add('fa-check');
+  div.appendChild(icon1);
+  const contianer = document.querySelector('#no_status');
+  contianer.appendChild(div);
   div.addEventListener("dragstart", dragStart);
-  div.addEventListener("dragend", dragEnd);
-  }
+  div.addEventListener("dragend", dragEnd); 
+  div.addEventListener("onclick",displayfun);
+  let form = document.getElementById("form");
+  form.dataset.taskId = uniqueId;
+  window["task-name"].value ="";
+  window["task-title"].value ="";
+  window["task-date"].value ="";
+  window["task-description"].value ="";
+  window["task-priority"].value ="";
+  show();
+  enable()
+}
+function deleteItem(){
+  alert("Hii!! I am ready for delete");
 }
 /*------------------Form------------------------ */
-let x=document.getElementById("form");
 function show(){
-  if(x.style.display==="none"){
-    x.style.display="block"
-    x.style.top="14%"
-  }else{
-    x.style.display="none"
-  }
+  let form = document.getElementById("form");
+  form.classList.remove('hide-form')
 }
-function cancel(){
-  if(x.style.display==="block"){
-    x.style.display="none"
-    x.style.top="-100%"
+
+function showWithDetails(element){
+  const id = element.dataset.id;
+  const task = data.find(item => item.id === id);
+  window["task-name"].innerText = task.name;
+  window["task-title"].value = task.title;
+  window["task-date"].value = task.date;
+  window["task-description"].value = task.description;
+  window["task-priority"].value = task.taskPriority;
+  show();
+  displayfun();
+}
+
+window["save-task-data"].addEventListener('click', (e) => {
+  e.preventDefault();
+  let id = e.target.parentNode.parentNode.parentNode.dataset.taskId;
+  let name = window["task-name"].value.trim();
+  let title = window["task-title"].value.trim();
+  let date = window["task-date"].value.trim();
+  let description = window["task-description"].value.trim();
+  let taskPriority = window["task-priority"].value.trim();
+  data.push(
+    {
+      id,
+      name,
+      title,
+      date,
+      description,
+      taskPriority
+    }
+  )
+  let form = document.getElementById("form");
+  form.classList.add('hide-form');
+})
+/*-------------------Input Box Disabled----------------------------- */
+ let temp=document.querySelectorAll(".input-disable");
+function displayfun(){
+   temp.forEach((e)=>{
+     e.disabled = true;
+   })
+   }
+ function enable(){
+   temp.forEach((e)=>{
+     e.disabled = false;
+   })
+ }
+ 
+function randomId(){
+  let id = "";
+  for(let i=0;i<9;i++){
+    id += Math.floor(Math.random() * 9);
   }
+  return id;
 }
